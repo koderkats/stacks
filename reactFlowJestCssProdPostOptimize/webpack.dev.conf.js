cf7358ee915b5071747265b6a9ebce140cb36ee6
me <49545028+parismaris@users.noreply.github.com>
2019-04-16 01:01:37 -0400

    init-reactFlowJestCssProdPostOptimize

---
+++ reactFlowJestCssProdPostOptimize/webpack.dev.conf.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+
+const folder = {
+  asset: path.resolve(__dirname, 'asset'),
+  dist: path.resolve(__dirname, 'distDev'),
+}
+
+module.exports = {
+  mode: 'development',
+
+  entry: ['./src/index.js'],
+  output: {
+    // publicPath: './',  // Required for: yarn build:dev
+    publicPath: '/',      // Required for: yarn start
+    path: folder.dist,
+    filename: '[name].bundle.js',
+  },
+
+  devtool: 'cheap-module-eval-source-map',
+
+  module: {
+    rules: [
+      {
+        exclude: /node_modules|packages/,
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        exclude: /node_modules|packages/,
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9',
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          }
+
+        ],
+      }
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin()
+  ],
+
+  devServer: {
+    contentBase: folder.asset,
+    index: 'index.html',
+    hot: true
+  },
+
+}
