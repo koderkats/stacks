c34d690841deb8d60aceaf1568742c1aa6ae2138
parismaris <49545028+parismaris@users.noreply.github.com>
2019-04-16 01:05:13 -0400

    init-reactFlowJestCssProdPostOptimize (#7)

---
+++ reactFlowJestCssProdPostOptimize/webpack.prod.conf.js
@@ -0,0 +1,93 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const folder = {
+  asset: path.resolve(__dirname, 'asset'),
+  dist: path.resolve(__dirname, 'dist'),
+}
+
+module.exports = {
+  mode: 'production',
+
+  entry: ['./src/index.js'],
+  output: {
+    publicPath: './',
+    path: folder.dist,
+    filename: '[name].bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        exclude: /node_modules|packages/,
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          }
+        ],
+
+      }
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    })
+  ],
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      }),
+      new OptimizeCSSAssetsPlugin()
+    ]
+  },
+
+
+}
+
